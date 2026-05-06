e7e53c5596666199026471896deb5441a44e6b1e
sineth <sinethnethila12@gmail.com>
2026-05-06 14:52:29 +0530

    modified Admin in  model

--- src/main/java/food_delivery_system/model/Admin.java
+++ src/main/java/food_delivery_system/model/Admin.java
@@ -1,22 +1,21 @@
 package food_delivery_system.model;
 
-public class Admin extends User {
+// OOP: Encapsulation
+public class Admin {
 
-    private String phone;
+    private String username;
+    private String password;
 
-    public Admin() {
+    public Admin() {}
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public Admin(String id, String name, String email, String password, String role, String phone) {
-        super(id, name, email, password, role);
-        this.phone = phone;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
